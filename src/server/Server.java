b3068760645587ce4823144f62693814e91a5c1e
Boris_Bokey <kimikorrkey@gmail.com>
2020-05-21 21:08:39 +0300

    fkfkj

--- src/server/Server.java
+++ src/server/Server.java
@@ -7,28 +7,30 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
-
+		// порт, который будет прослушивать наш сервер
     static final int PORT = 3443;
-
+		// список клиентов, которые будут подключаться к серверу
     private ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>();
 
     public Server() {
-
+				// сокет клиента, это некий поток, который будет подключаться к серверу
+				// по адресу и порту
         Socket clientSocket = null;
-				// сервер сокет
+				// серверный сокет
         ServerSocket serverSocket = null;
         try {
-
+						// создаём серверный сокет на определенном порту
             serverSocket = new ServerSocket(PORT);
             System.out.println("Сервер запущен!");
-
+						// запускаем бесконечный цикл
             while (true) {
-
+								// таким образом ждём подключений от сервера
                 clientSocket = serverSocket.accept();
-
+								// создаём обработчик клиента, который подключился к серверу
+								// this - это наш сервер
                 ClientHandler client = new ClientHandler(clientSocket, this);
                 clients.add(client);
-
+								// каждое подключение клиента обрабатываем в новом потоке
                 new Thread(client).start();
             }
         }
@@ -37,7 +39,7 @@ public class Server {
         }
         finally {
             try {
-
+								// закрываем подключение
                 clientSocket.close();
                 System.out.println("Сервер остановлен");
                 serverSocket.close();
@@ -47,8 +49,8 @@ public class Server {
             }
         }
     }
-
-
+		
+		// отправляем сообщение всем клиентам
     public void sendMessageToAllClients(String msg) {
         for (ClientHandler o : clients) {
             o.sendMsg(msg);
@@ -56,7 +58,7 @@ public class Server {
 
     }
 
-
+		// удаляем клиента из коллекции при выходе из чата
     public void removeClient(ClientHandler client) {
         clients.remove(client);
     }
